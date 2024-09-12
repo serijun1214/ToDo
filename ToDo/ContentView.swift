8fb07caa863ca3472b6ad78432dd635c3b0cc443
serijun1214 <serijun.1214+github@gmail.com>
2024-09-13 08:31:34 +0900

    簡易的なリスト操作を実装

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -8,17 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var taskData = [(title: "ジョギングする", completed: true),
+                           (title: "お花に水をやる", completed: false),
+                           (title: "部屋の掃除をする", completed: false),
+                           (title: "本を読む", completed: false),
+                           (title: "トイレ掃除", completed: false)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(0..<taskData.count, id: \.self) { index in
+                Button {
+                    taskData[index].completed.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: taskData[index].completed ? "checkmark.circle.fill" : "circle")
+                        Text(taskData[index].title)
+                    }
+                }
+                .foregroundColor(.primary)
+            }
+            .navigationTitle("ToDoリスト")
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
